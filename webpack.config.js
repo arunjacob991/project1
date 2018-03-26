8f78f234cb0f6229e768c6aef4d486bd60bb9f38
Jibin George <mail@jibingeorge.com>
2018-03-26 13:23:29 +0530

    next commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './src/test.js',
   output: {
-    filename: 'dist/bundle.js'
+    filename: 'dist/bundle.js',
   },
   module: {
     rules: [
@@ -9,33 +9,33 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [{loader: 'style-loader'} , {loader:'css-loader'} ]
-      }
-    ]
-  }
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
   module: {
-   rules: [
-     {
-       test: /\.(png|jpg|gif)$/,
-       use: [
-         {
-           loader: 'file-loader',
-           options: {}
-         }
-       ]
-     }
-   ]
- }
- module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  module: {
     rules: [
       {
         test: /\.(png|jpg|gif)$/,
@@ -43,11 +43,11 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8192
-            }
-          }
-        ]
-      }
-    ]
-  }
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
